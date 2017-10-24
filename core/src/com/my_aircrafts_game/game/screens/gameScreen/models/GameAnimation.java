03a43a1a7ce166dfc79a259389ea94340335f004
Igor <1itcode1@gmail.com>
2017-10-24 11:49:58 +0300

    create UI live line, refactor GameAnimation

--- core/src/com/my_aircrafts_game/game/screens/gameScreen/models/GameAnimation.java
+++ core/src/com/my_aircrafts_game/game/screens/gameScreen/models/GameAnimation.java
@@ -4,34 +4,22 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Pool;
 
 
-public class GameAnimation implements Pool.Poolable {
-    private Vector2 position;
+public class GameAnimation {
     private boolean active;
     private float time;
     private int key;
     private int frames;
-    private Vector2 velocity;
-
 
     public GameAnimation() {
-        position = new Vector2(0f, 0f);
-        velocity = new Vector2(0f, 0f);
     }
 
-    public void init(int frames, Vector2 position) {
-        this.position.set(position);
+    public void init(int frames) {
         this.frames = frames;
         active = true;
         time = 0f;
     }
 
-    public void init(int frames, Vector2 position, Vector2 velocity) {
-        this.velocity = velocity;
-        init(frames, position);
-    }
-
     public void update(float deltaTime) {
-        position.mulAdd(velocity, deltaTime);
         if (key < frames) {
             time += deltaTime * 5;
             key = (int) time;
@@ -43,34 +31,17 @@ public class GameAnimation implements Pool.Poolable {
         }
     }
 
-    @Override
     public void reset() {
         key = 0;
         frames = 0;
         time = 0f;
-        position.set(0f, 0f);
     }
-
-    public Vector2 getPosition() {
-        return position;
-    }
-
-    public void setActive(boolean isActive) {
-        this.active = isActive;
-    }
-
 
     public int getFrameNumber() {
         return key;
     }
 
-    public void setPosition(Vector2 position) {
-        this.position = position;
-    }
-
     public boolean isActive() {
         return active;
     }
-
-
 }
